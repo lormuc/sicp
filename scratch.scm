bc925e639ea4836495e0c7bf4e1becbf0462ed65
Veniamin Petrenko <benjamin.petrenko@gmail.com>
2020-06-30 15:46:03 +0300

    has-cycle and 3.21

--- scratch.scm
+++ scratch.scm
@@ -283,4 +283,71 @@
     (count-pairs w)))
  )
 
+(define (has-cycle y)
+  (let ((visited-pairs '()))
+    (define (helper x)
+      (cond ((null? x)
+             #f)
+            ((memq x visited-pairs)
+             #t)
+            (else
+             (set! visited-pairs (cons x visited-pairs))
+             (helper (cdr x)))))
+    (helper y)))
+
+(test-group
+ "has-cycle"
+ (test
+  #f
+  (let ((x '(0 1 2)))
+    (has-cycle x)))
+ (test
+  #t
+  (let ((x (cons 0 0)))
+    (set-cdr! x x)
+    (has-cycle x))))
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "front called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "delete! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
+
+(define (print-queue queue)
+  (display (front-ptr queue)))
+
+(define queue (make-queue))
+(insert-queue! queue 'a)
+(insert-queue! queue 'b)
+(insert-queue! queue 'c)
+(insert-queue! queue 'd)
+(print-queue queue)
+(newline)
+(delete-queue! queue)
+
 (define debug #t)
