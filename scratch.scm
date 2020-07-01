5d1824454311edded9dd70450fed11cfc512d496
Veniamin Petrenko <benjamin.petrenko@gmail.com>
2020-07-01 16:41:10 +0300

    3.29 or-gate with and-gate and inverter

--- scratch.scm
+++ scratch.scm
@@ -712,16 +712,15 @@
       1
       0))
 
-(define (or-gate a1 a2 output)
-  (define (and-action-procedure)
-    (let ((new-value
-           (logical-or (get-signal a1) (get-signal a2))))
-      (after-delay and-gate-delay
-                   (lambda ()
-                     (set-signal! output new-value)))))
-  (add-action! a1 and-action-procedure)
-  (add-action! a2 and-action-procedure)
-  'ok)
+(define (or-gate x y output)
+  (let ((inv-x (make-wire))
+        (inv-y (make-wire))
+        (inv-and (make-wire)))
+    (inverter x inv-x)
+    (inverter y inv-y)
+    (and-gate inv-x inv-y inv-and)
+    (inverter inv-and output)
+    'ok))
 
 (test-group
  "or-gate"
