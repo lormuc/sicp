af19a04f0cc9c53a752d3f77af356b9de6defb9c
Veniamin Petrenko <benjamin.petrenko@gmail.com>
2020-07-03 14:43:00 +0300

    3.50 stream-map

--- scratch.scm
+++ scratch.scm
@@ -1097,4 +1097,97 @@
         (set-value! f 212 'user)
         (get-value c))))))
 
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((cons-stream x y) (cons x (delay y)))))
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define stream-null? null?)
+(define the-empty-stream '())
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (display x)
+  (newline))
+
+(define (stream-x-map proc . streams)
+  (if (null? (car streams))
+      the-empty-stream
+      (cons-stream (apply proc (map stream-car streams))
+                   (apply stream-x-map
+                          (cons proc
+                                (map stream-cdr streams))))))
+
+(test-group
+ "stream-x-map"
+ (test
+  '(2 4)
+  (let ((stream (stream-x-map
+                 (lambda (x) (+ 1 x))
+                 (cons-stream 1
+                              (cons-stream 3
+                                           the-empty-stream)))))
+    (list (stream-car stream)
+          (stream-car (stream-cdr stream)))))
+ (test
+  '(3 1)
+  (let ((stream
+         (stream-x-map
+          +
+          (cons-stream 0 (cons-stream 0 the-empty-stream))
+          (cons-stream 1 (cons-stream 0 the-empty-stream))
+          (cons-stream 2 (cons-stream 1 the-empty-stream)))))
+    (list (stream-car stream)
+          (stream-car (stream-cdr stream)))))
+ (test
+  #t
+  (stream-null?
+   (stream-x-map - the-empty-stream the-empty-stream))))
+
 (define debug #t)
