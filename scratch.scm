6bad9b84687173a7b2b4de40e339e89111b355a9
Veniamin Petrenko <benjamin.petrenko@gmail.com>
2020-07-15 18:32:40 +0300

    4.38

--- scratch.scm
+++ scratch.scm
@@ -1544,17 +1544,32 @@
      (amb low (an-integer-between (+ low 1) high)))
    (define (an-integer-starting-from low)
      (amb low (an-integer-starting-from (+ 1 low))))
-   (define (a-pythagorean-triple k-low)
-     (amb (let ((i (an-integer-between 1 k-low)))
-            (let ((j (an-integer-between i k-low)))
-              (require (= (+ (* i i) (* j j)) (* k-low k-low)))
-              (list i j k-low)))
-          (a-pythagorean-triple (+ 1 k-low))))
-   (a-pythagorean-triple 0)
-   try-again
-   try-again
-   try-again
-   try-again
+   (define (distinct? items)
+     (cond ((null? items) true)
+           ((null? (cdr items)) true)
+           ((member (car items) (cdr items)) false)
+           (else (distinct? (cdr items)))))
+   (define (multiple-dwelling)
+     (let ((baker (amb 1 2 3 4 5))
+           (cooper (amb 1 2 3 4 5))
+           (fletcher (amb 1 2 3 4 5))
+           (miller (amb 1 2 3 4 5))
+           (smith (amb 1 2 3 4 5)))
+       (require
+        (distinct? (list baker cooper fletcher miller smith)))
+       (require (not (= baker 5)))
+       (require (not (= cooper 1)))
+       (require (not (= fletcher 5)))
+       (require (not (= fletcher 1)))
+       (require (> miller cooper))
+       ;; (require (not (= (abs (- smith fletcher)) 1)))
+       (require (not (= (abs (- fletcher cooper)) 1)))
+       (list (list 'baker baker)
+             (list 'cooper cooper)
+             (list 'fletcher fletcher)
+             (list 'miller miller)
+             (list 'smith smith))))
+   (multiple-dwelling)
    try-again
    try-again
    try-again
